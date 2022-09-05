2db7c560b454ba1dc88b74832bc3fd91c6f3c457
tanmaybhandge <111745693+tanmaybhandge@users.noreply.github.com>
2022-09-05 12:56:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = "1.7.0"
+    }
+  }
+}
+
+data "nutanix_cluster" "cluster" {
+  name = var.cluster_name
+}
+data "nutanix_subnet" "subnet" {
+  subnet_name = var.subnet_name
+}
+
+variable "image_name_webserver" {
+    type = string
+    default = "ubuntu_final"
+  
+}
+
+## Web Server Fetching the image information ##
+
+data "nutanix_image" "image_info_webserver" {
+  image_name_webserver = var.image_name_webserver
+}
+
+provider "nutanix" {
+  username     = var.user
+  password     = var.password
+  endpoint     = var.endpoint
+  insecure     = true
+  wait_timeout = 60
+}
+
+resource "nutanix_image" "image" {
+  name        = "Arch Linux"
+  description = "Arch-Linux-x86_64-basic-20210401.18564"
+  source_uri  = "https://mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-basic-20210415.20050.qcow2"
+}
+
+resource "nutanix_virtual_machine" "vm" {
+  name                 = "MyVM from the Terraform Nutanix Provider"
+  cluster_uuid         = data.nutanix_cluster.cluster.id
+  num_vcpus_per_socket = "2"
+  num_sockets          = "1"
+  memory_size_mib      = 1024
+
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = nutanix_image.image.id
+    }
+  }
+
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = data.nutanix_image.image_info_webserver.id
+    }
+
+    device_properties {
+      disk_address = {
+        device_index = 0
+        adapter_type = "SCSI"
+      }
+    }
+  }
+
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.subnet.id
+  }
+}
